feffca8bf47a0b1b6ae819f16ad15ec14a9eda76
Ivan Anto <riquadro2012@gmail.com>
2019-11-15 15:46:59 +0200

    Code refactoring
    Add dedicated function `get_command_output` which returns command
    output and process exceptions related to this command.
    
    Transform rpm resource retrieving into the dedicated function
    `get_rpm_resources_list` for a more readable script entrypoint.
    
    Transform rpm resource list processing into the dedicated function
    `process_rpm_resources_list`. Add check which skips a dependency not
    present in the yum repositories.
    
    Change some variable names for more readable code.

--- dep-checker.py
+++ dep-checker.py
@@ -26,6 +26,16 @@ def parse_args(arguments):
 
     return parser.parse_args(arguments[1:])
 
+def get_command_output(command):
+    try:
+        output = subprocess.check_output(command, universal_newlines=True)
+        return output
+    except subprocess.CalledProcessError as cpe:
+        print(command)
+        print(cpe.output)
+        print('Command error code: {}'.format(cpe.returncode))
+        exit(1)
+
 
 def get_resource_packages(output):
     # find lines with package info (e.g. bash-4.2.46-33.el7.x86_64 : The GNU Bourne Again shell)
@@ -41,35 +51,43 @@ def get_resource_packages(output):
     return resource_packages_list
 
 
-def extract_package_name(full_name):
-    return full_name.rsplit('-', 2)[0]
+def extract_package_name(full_package_name):
+    return full_package_name.rsplit('-', 2)[0]
 
 
-def get_installed_package(pac_name):
-    full_package_name = subprocess.check_output(["rpm", "-qv", pac_name], universal_newlines=True).rstrip()
+def get_installed_package(package_name):
+    command_exec = ["rpm", "-qv", package_name]
+    full_package_name = get_command_output(command_exec).rstrip()
     return full_package_name
 
-if __name__ == "__main__":
-    argv = sys.argv
-    args = parse_args(argv)
-    package_to_check = args.package
 
-    # TODO: add error handling when package not found
-    command = subprocess.check_output(["rpm", "-qR", package_to_check], universal_newlines=True)
+def get_rpm_resources_list(package_name):
+    command_exec = ["rpm", "-qR", package_name]
+    resources_list_output = get_command_output(command_exec)
 
     # create a list of package resources and remove duplicates from this list
-    rpm_resources_list = list(dict.fromkeys(command.splitlines()))
+    resources_list = list(dict.fromkeys(resources_list_output.splitlines()))
+    return resources_list
 
-    for resource in rpm_resources_list:
+
+def process_rpm_resources_list(resources_list):
+    for resource in resources_list:
         # skip rpm resources
         if 'rpmlib' in resource:
             continue
-        # TODO: add error handling when information about dependency not found
-        command = subprocess.check_output(["yum", "provides", "{}".format(resource)], universal_newlines=True)
+
+        command_exec = ["yum", "provides", "{}".format(resource)]
+        dependency_packages_output = get_command_output(command_exec)
+
+        # skip dependency if information about it doesn't present in the yum repos
+        if 'No matches found' in dependency_packages_output:
+            print(command_exec)
+            print('Resource "{}" | No matches found in repositories\n'.format(resource))
+            continue
 
         # get resource packages names from the yum output (e.g.
         # ['procps-ng-3.3.10-26.el7.i686', 'procps-ng-3.3.10-26.el7.x86_64', 'procps-ng-3.3.10-26.el7_7.1.i686'])
-        resource_packages = get_resource_packages(command)
+        resource_packages = get_resource_packages(dependency_packages_output)
 
         # get short package name (e.g. openssl or procps-ng)
         pack_name = extract_package_name(resource_packages[0])
@@ -85,3 +103,13 @@ if __name__ == "__main__":
             if pack_name in resource_packages[0]:
                 print('Installed package: ' + installed_package + '!!!')
         print()
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    args = parse_args(argv)
+    package_to_check = args.package
+
+    rpm_resources_list = get_rpm_resources_list(package_to_check)
+
+    process_rpm_resources_list(rpm_resources_list)
